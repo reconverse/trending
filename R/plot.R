59659064a3298f8dc0d315a46526b1b1b8bfc9b1
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2020-08-05 15:17:19 +0100

    plot fixes for R CMD check

--- R/plot.R
+++ R/plot.R
@@ -1,26 +1,38 @@
+#' Plot
+#'
+#' Plot an object of class `fitted_and_predicted`.
+#'
+#' @param x A [fit_and_predict()] object.
+#' @param date_axis Variable in x representing the date axis.
+#' @param known_y Variable in x representing the known counts.
+#' @param predicted_y Variable in x representing the predicted counts.
+#' @param lower_y Variable in x representing the lower prediction interval.
+#' @param upper_y Variable in x representing the upper prediction interval.
+#' @param ... Not currently used.
+#'
 #' @export
-plot.fitted_and_predicted <- function(dat, x_axis, known_y, predicted_y = "pred",
-                                 lower_y = "lower", upper_y = "upper") {
-  known_dat <- dat[is.na(dat[[predicted_y]]), ]
-  pred_dat <- dat[!is.na(dat[[predicted_y]]), ]
+plot.fitted_and_predicted <- function(x, date_axis, known_y, predicted_y = "pred",
+                                 lower_y = "lower", upper_y = "upper", ...) {
+  known_dat <- x[is.na(x[[predicted_y]]), ]
+  pred_dat <- x[!is.na(x[[predicted_y]]), ]
 
-  last_date <- as.integer(max(known_dat[[x_axis]]))
-  first_date <- as.integer(min(pred_dat[[x_axis]]))
+  last_date <- as.integer(max(known_dat[[date_axis]]))
+  first_date <- as.integer(min(pred_dat[[date_axis]]))
 
   (first_date - last_date) / 2
 
-  ggplot2::ggplot(dat, ggplot2::aes(x = .data[[x_axis]])) +
+  ggplot2::ggplot(x, ggplot2::aes(x = .data[[date_axis]])) +
     ggplot2::theme_bw() +
     ggplot2::geom_col(ggplot2::aes(y = .data[[known_y]]),
                       fill = "#0077BB",
                       alpha = 0.7,
                       color = "white") +
-    ggplot2::geom_ribbon(ggplot2::aes(x = .data[[x_axis]],
+    ggplot2::geom_ribbon(ggplot2::aes(x = .data[[date_axis]],
                                       ymin = .data[[lower_y]],
                                       ymax = .data[[upper_y]]),
                          fill = "#BBB67E",
                          alpha = 0.4) +
-    ggplot2::geom_point(ggplot2::aes(x = .data[[x_axis]],
+    ggplot2::geom_point(ggplot2::aes(x = .data[[date_axis]],
                                      y = .data[[predicted_y]])) +
     ggplot2::geom_vline(xintercept = (first_date + last_date) / 2, linetype = 2) +
     ggplot2::xlab("")
