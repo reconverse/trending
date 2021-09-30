5f1754d6c3270bf26d205e56a73d10f8e2a9df10
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2021-09-30 14:41:00 +0100

    Formatting tweaks

--- R/imports.R
+++ R/imports.R
@@ -1,11 +1,16 @@
+#' @importFrom stats AIC family fitted formula gaussian predict
+#' @importFrom stats qnbinom qpois qgamma qbinom qt setNames
+#' @importFrom MASS glm.nb
+NULL
+
+#' @export
+MASS::glm.nb
+
 #' @importFrom ciTools add_ci add_pi
+NULL
+
 #' @importFrom pillar style_subtle
-#' @importFrom stats AIC family fitted formula gaussian predict setNames
-#' @importFrom stats qnbinom qpois qgamma qbinom qt
 #' @importFrom tibble as_tibble new_tibble tibble validate_tibble
 #' @importFrom vctrs new_data_frame
 NULL
 
-#' @importFrom MASS glm.nb
-#' @export
-MASS::glm.nb
